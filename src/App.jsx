45095a2503838e0082d21aad8425748ac2ab39e8
Marcio Marçal <Kayquehenrryfo@gmail.com>
2024-03-27 23:35:47 -0300

    Projeto finalizado <3

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Luxury from "./components/Luxury"
 export default function app() {
   return(
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
-      <section className="lg:flex max-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         <Sedans/>
         <SUVs/>
         <Luxury/>
-      </section>
+      </div>
     </main>
   )
 }
